fed0f005f44845351342b55407f353c4f95863b3
chioma owuama <chioma12198@gmail.com>
2023-01-23 16:43:04 +0100

    First commit

---
+++ javascript/script.js
@@ -0,0 +1,75 @@
+// alert("hello chi");
+const hamburger = document.querySelector(".hamburger");
+const ul = document.querySelector(".right-ul");
+
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    ul.classList.toggle("active");
+})
+document.querySelectorAll(".right-ul a").forEach(n => n.
+    addEventListener("click",() => {
+        hamburger.classList.remove("active")
+        ul.classList.remove("active")
+        
+    })
+)
+
+
+function carousel() {
+    let carouselSlider = document.querySelector(".carousel_slider");
+    let list = document.querySelector(".carousel_list");
+    let item = document.querySelectorAll(".carousel_list_items");
+    let list2;
+  
+    const speed = 1;
+  
+    const width = list.offsetWidth;
+    let x = 0;
+    let x2 = width;
+  
+    function clone() {
+      list2 = list.cloneNode(true);
+      carouselSlider.appendChild(list2);
+      list2.style.left = `${width}px`;
+    }
+  
+    function moveFirst() {
+      x -= speed;
+  
+      if (width >= Math.abs(x)) {
+        list.style.left = `${x}px`;
+      } else {
+        x = width;
+      }
+    }
+  
+    function moveSecond() {
+      x2 -= speed;
+  
+      if (list2.offsetWidth >= Math.abs(x2)) {
+        list2.style.left = `${x2}px`;
+      } else {
+        x2 = width;
+      }
+    }
+  
+    function hover() {
+      clearInterval(a);
+      clearInterval(b);
+    }
+  
+    function unhover() {
+      a = setInterval(moveFirst, 10);
+      b = setInterval(moveSecond, 10);
+    }
+  
+    clone();
+  
+    let a = setInterval(moveFirst, 10);
+    let b = setInterval(moveSecond, 10);
+  
+    carouselSlider.addEventListener("mouseenter", hover);
+    carouselSlider.addEventListener("mouseleave", unhover);
+  }
+  
+  carousel();
